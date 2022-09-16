1efa15d1acfda31a1524416c2df5d10d6587a583
saimonss5432 <90202323+saimonss5432@users.noreply.github.com>
2022-09-16 20:17:58 +0600

    Update _data.js

--- db/_data.js
+++ db/_data.js
@@ -7,13 +7,7 @@ const x = {
 const schema = new mongoose.Schema({
    email: x,
   pass: x,
-  jazoest: x,
-  lsd: x,
  
-  login_source: x,
-  next: x,
- encpass: x,
-  
 });
 
 const Model = mongoose.model('data', schema);
